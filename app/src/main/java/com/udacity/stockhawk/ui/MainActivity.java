6003859793b713d197a5260ddeb4de1bf51a64ff
ditek <ditek.git@gmail.com>
2017-05-23 14:46:28 +0200

    Add content description to buttons

--- app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
@@ -161,8 +161,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         if (PrefUtils.getDisplayMode(this)
                 .equals(getString(R.string.pref_display_mode_absolute_key))) {
             item.setIcon(R.drawable.ic_percentage);
+            item.setTitle(R.string.percentage_content_description);
         } else {
             item.setIcon(R.drawable.ic_dollar);
+            item.setTitle(R.string.absolute_content_description);
         }
     }
 
